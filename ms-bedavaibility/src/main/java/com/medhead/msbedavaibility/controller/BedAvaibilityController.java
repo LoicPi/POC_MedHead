901319f757327692f6743039239e6df870bc8fd8
Loïc <lcpiriou@gmail.com>
2023-03-01 10:28:49 +0100

    Code Fix : Change on function in controller the way to call it

--- ms-bedavaibility/src/main/java/com/medhead/msbedavaibility/controller/BedAvaibilityController.java
+++ ms-bedavaibility/src/main/java/com/medhead/msbedavaibility/controller/BedAvaibilityController.java
@@ -2,7 +2,7 @@ package com.medhead.msbedavaibility.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.medhead.msbedavaibility.service.BedAvaibilityService;
@@ -13,8 +13,8 @@ public class BedAvaibilityController {
     @Autowired
     private BedAvaibilityService bedAvaibilityService;
 
-    @PostMapping("/savedBedAvaibility")
-    public String savedBedAvaibility(@RequestBody Integer hospitalId) {
+    @PostMapping("/savedBedAvaibility/{hospitalId}")
+    public String savedBedAvaibility(@PathVariable(value="hospitalId") Integer hospitalId) {
         return bedAvaibilityService.savedBedAvaibility(hospitalId);
     }
 }
